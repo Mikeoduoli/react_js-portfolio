7c853072472e6ffac030ca1ad2f94e84e517cc66
Mikeoduoli <mikeoduoli@gmail.com>
2023-12-31 13:52:10 +0300

    React Tailwind Crash Coourse

---
+++ src/components/About.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import aboutImg from '../assets/images/about.png'
+
+const About = () => {
+    const info =[
+        {text: 'Years Experience', count: '02'},
+        {text: 'Completed Projects', count: '20'},
+        {text: 'Companies Work', count: '02'},
+    ]
+    return
+        <section id="about" className="py-10 text-white">
+            <div className="text-center mt-8">
+                <h3 className="text-4xl font-semibold">
+                    About <span className="text-cyan-600">Me</span>
+                </h3>
+                <p className="text-gray-400 my-3 text-lg">Introduction</p>
+                <div className="flex md:flex-row flex-col-reverse items-center
+                 md:gap-6 gap-12 px-10 max-w-6xl mx-auto ">
+                    <div className="p-2">
+                        <div className="text-gray-300 my-3">
+                            <p className="text-justify leading-7 w-11/12 mx-auto">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores dignissimos id nam in rem reprehenderit quis, illo dolores minus numquam molestiae enim, ullam laudantium eaque odio corporis at iusto dolorem!
+                            </p>
+                            <div className="flex mt-10 items-center gap-7">
+                                {
+                                  info.map(content=>(
+                                    <div key={content.text}>
+                                        <h3 className="md:text-4xl text-2xl font-semibold
+                                         text-white">
+                                        {content.count}
+                                        <span className="text-cyan-600">+</span>{" "}
+                                        </h3>
+                                    <span className="md:text-base text-xs">{content.text}</span>
+                                    </div>
+                                  ))
+                                }
+                            </div>
+                            <br />
+                            <br />
+                            <a href="src/assets/Michael_Obed_Oduoli_DataAnalystResume.pdf" download>
+                            <button className="btn-primary mt-5">Download Resume</button>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="flex-1 md:mt-0 mt-6 flex justify-center items-center">
+                        <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12
+                        max-w-sm aboutImg">
+                            <img src={aboutImg} 
+                             alt="" className="w-full object-cover
+                             bg-cyan-600 rounded-xl"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>;
+};
+
+export default About;
---
+++ src/components/Project.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Swiper, SwiperSide } from "swiper/react";
+import project1 from "../assets/images/project-1.jpg";
+import project2 from "../assets/images/project-2.jpg";
+import project3 from "../assets/images/project-3.jpg";
+import project4 from "../assets/images/project-4.jpg";
+import project5 from "../assets/images/project-5.jpg";
+import project_person from "../assets/images/project_person1.png";
+
+import "swiper/css"
+import "swiper/css/pagination"
+import {Pagination, Autoplay} from 'swiper'
+
+const Project = () => {
+    const projects = [
+        {img: project1, name: 'Movie App', github_link : 'https://github.com/Sridhar-C-25', live_link:'https://myreactflix.netflix.netlify.app'},
+        {img: project2, name: 'Job search Web', github_link : 'https://github.com/Sridhar-C-25/jobsearchapp', live_link:'https://myjobsearch.netlify.app'},
+        {img: project3, name: 'Highking', github_link : 'https://github.com/Sridhar-C-25/hiking', live_link:'https://hiking01.netlify.app'},
+        {img: project4, name: 'React Nav', github_link : 'https://github.com/Sridhar-C-25/reacttailwindmavbar-wth-dropdown', live_link:'https://reacttailwindnavbar.netlify.app'},
+        {img: project5, name: 'Vue Country', github_link : 'https://github.com/Sridhar-C-25', live_link:'https://vuecountry05.netlify.app'}
+    ];
+    return (
+        <section id="projects" className="py-10 text-white">
+        <div className="text-center">
+            <h3 className="text-4xl font-semibold">
+                My <span>Projects</span>
+            </h3>
+            <p className="text-gray-400 mt-3 text-lg">Awesome Work Delivered</p>
+        </div>
+        <br />
+        <div className="flex max-w-6xl px-5 mx-auto items-center relative">
+            <div className="lg:w-2/3 w-full">
+                <swiper slidesPerview={1.2} spaceBetween={20} breakpoints={{
+                    768: {
+                        slidesPerview: 2,
+                    }
+                }}
+                loop={true} autoplay={{
+                    delay: 3000,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={{Pagination, Autoplay}}>
+                    {
+                        projects.map((project_info, i) => (
+                            <SwiperSide key={i}>
+                                <div className="h-fit w-full p-4 bg-slate-700 rounded-xl ">
+                                    <img src={project_info.img} alt='' className="rounded-lg"/>
+                                    <h3 className="">{project_info.name}</h3>
+                                    <div className="flex gap-3">
+                                        <a href= {project_info.github_link} target = "_blank" className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block">Github</a>
+                                        <a href={project_info.live_link} className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block">Live Demo</a>
+                                    </div>
+                                </div>
+                            </SwiperSide>
+                        ))}
+                </swiper>
+            </div>
+            <div className="lg:block hidden">
+                <img src={project_person} alt=""/>
+            </div>
+        </div>
+    </section>
+    );
+};
+
+export default Project;
